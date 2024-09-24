1a6c06eada1a95905c58243e8df57e0837a790ab
jheca444 <jzarta11@itfip.edu.co>
2024-09-23 19:04:02 -0500

    Todos los trabajos

---
+++ trabajosplantillas/dashboard/public/menu.php
@@ -0,0 +1,47 @@
+<div class="container-fluid">
+  <div class="row">
+    <div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
+      <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="sidebarMenuLabel">Company name</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+          <ul class="nav flex-column">
+          <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="index.php">
+                <svg class="bi"><use xlink:href="#puzzle"/></svg>
+                Inicio
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="Informacion_y_ayuda.php">
+                <svg class="bi"><use xlink:href="#house-fill"/></svg>
+                Informacion y ayuda
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="Fecha_y_hora.php">
+                <svg class="bi"><use xlink:href="#file-earmark"/></svg>
+                Fecha y hora
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="Reservas.php">
+                <svg class="bi"><use xlink:href="#cart"/></svg>
+               Reservas
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="Tu_reserva.php">
+                <svg class="bi"><use xlink:href="#people"/></svg>
+               Tu reserva
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="Ofertas_y_destinos.php">
+                <svg class="bi"><use xlink:href="#graph-up"/></svg>
+               Ofertas y destinos
+              </a>
+            </li>
+           
